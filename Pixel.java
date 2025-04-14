26ac173f6d01b6a9b6ae2e9e7cbfac8f1f485ab0
vitin2212 <137333989+vitin2212@users.noreply.github.com>
2025-04-14 19:45:01 -0300

    Update Pixel.java

--- Pixel.java
+++ Pixel.java
@@ -1,8 +1,11 @@
+// Representa um ponto com coordenadas x e y
 public class Pixel {
     int x, y;  
 
+    // Construtor
     public Pixel(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
+
